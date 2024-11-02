b159caab26c635930f04e05ea836fb5ebcccb456
Nicolau <nrocabus@uoc.edu>
2024-11-02 01:12:27 +0100

    Functional website. Stylelint ok

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,11 +6,33 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
+
+
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
+
+( function() {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+ 
+
+function toggleClass(idname, classname) {
+  let element = document.getElementById(idname);
+  element.classList.toggle(classname);
+}
+
+function expandCard(idname){
+  document.getElementById(idname).classList.toggle("info-card--active");
+  document.getElementById(idname+"_title").classList.toggle("info-card__title--active");
+  document.getElementById(idname+"_text").classList.toggle("info-card__text--active");
+}
+
+window.toggleClass = toggleClass;
+window.expandCard = expandCard;
